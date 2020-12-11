4df837090ea003a87ef61967ec4565b86a0275f1
goandreus <andres.chavez.crisostomo@gmail.com>
2020-12-11 16:44:13 -0500

    home package (#4)

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_ui_avanzadas/libs/auth.dart';
+import 'package:todo_project/libs/auth.dart';
 
 import '../../libs/auth.dart';
 
